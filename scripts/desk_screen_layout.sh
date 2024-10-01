869518da9420afafa4d4092f07b1605a70bcb2ad
Morten Eshøj <Mdue84@gmail.com>
2024-10-01 20:42:17 +0200

    Update desk_screen_layout.sh

--- scripts/desk_screen_layout.sh
+++ scripts/desk_screen_layout.sh
@@ -1,10 +1,10 @@
 
 # Set DP1 and DP2 to mirror at 3840x2160
-xrandr --output DP-2 --auto
+xrandr --output HDMI-1-0 --auto
 
 # Set the third display (HDMI-1) to 1920x1080 and position it to the right
-xrandr --output eDP-1 --mode 1920x1080 --rate 60 --right-of DP-2
+xrandr --output eDP --auto  --right-of DP-2
 
 # Set the wallpaper using feh
-feh --bg-scale ~/wallpapers/rick-and-morty-1.jpg
+feh --bg-scale ~/wallpapers/dune.jpg
 
