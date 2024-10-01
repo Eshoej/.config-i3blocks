0bf52a76231c8777beff97b4d2da5940b8b671ac
Morten Eshøj <Mdue84@gmail.com>
2024-10-01 20:50:01 +0200

    Update laptop_layout.sh

--- scripts/laptop_layout.sh
+++ scripts/laptop_layout.sh
@@ -1,6 +1,6 @@
-xrandr --output eDP-1 --mode 1920x1080 --rate 60 --scale 1x1
-xrandr --output DP-2 --off
+xrandr --output eDP --auto
+xrandr --output HM`DMI-1-0 --off
 
 # Set the wallpaper using feh
-feh --bg-scale ~/wallpapers/rick-and-morty-1.jpg
+feh --bg-scale ~/wallpapers/dune.jpg
 
